5173fd4265c60e96f4d6ca905b582db5bf50ebbe
Javier Rodriguez <amigojavier14@outlook.com>
2022-11-03 22:52:54 -0600

    SpringBootJPA

---
+++ src/main/java/com/example_jpa/ejercicio_jpa/EjercicioJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example_jpa.ejercicio_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example_jpa/ejercicio_jpa/EjercicioJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_jpa.ejercicio_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
